91eb70125bf7a3e4abf5a01351fbc11d25c730ce
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-14 18:27:13 +0530

    recursive fib

---
+++ Algorithms/Math/recursiveFib.js
@@ -0,0 +1,11 @@
+// given a number "n" , find the nth element of the fibonacci sequence
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(3));
+console.log(recursiveFibonacci(6));
