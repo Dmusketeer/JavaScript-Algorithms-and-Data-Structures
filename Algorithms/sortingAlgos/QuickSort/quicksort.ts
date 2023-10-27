12cfc4c4d04296fd669e8ba5c74d559f5800125c
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-27 11:48:40 +0530

    quick sort algorithm

---
+++ Algorithms/sortingAlgos/QuickSort/quicksort.ts
@@ -0,0 +1,25 @@
+const quickSort = (arr: number[]): number[] => {
+    if (arr.length < 2) {
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left: number[] = []
+    let right: number[] = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+
+
+// Example usage:
+const unsortedArray = [6, 3, 8, 5, 2, 7, 4, 1];
+const sortedArray = quickSort(unsortedArray);
+console.log(sortedArray); // Outputs: [1, 2, 3, 4, 5, 6, 7, 8]
+
+
