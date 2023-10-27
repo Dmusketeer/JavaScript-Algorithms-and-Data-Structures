7cb8b94f6d9d62b422f4d2248b5300795c23b690
Dmuskeeter <tdheeraj9a@gmail.com>
2023-10-27 16:39:44 +0530

    selection sort

---
+++ Algorithms/sortingAlgos/SelectionSort/selectionSort.ts
@@ -0,0 +1,23 @@
+const selectionSort = (arr: number[]): number[] => {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            const temp = arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp
+        }
+    }
+    return arr
+}
+
+
+// Example usage:
+const unsortedArray = [64, 25, 12, 22, 11];
+const sortedArray = selectionSort(unsortedArray);
+console.log(sortedArray); // Outputs: [11, 12, 22, 25, 64]
